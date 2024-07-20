75005212656d102eb2b746c1ecace752fb9022ff
swapna <swapnabgg97@gmail.com>
2024-07-20 05:52:36 +0530

    moenage

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/Users')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/mo")
+
+app.get('/',(req,res) => {
+    UserModel.find({})
+    .then(users=>res.json(users))
+    .catch(err => res.json(err))
+})
+
+
+app.post("/createUser",(req,res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
